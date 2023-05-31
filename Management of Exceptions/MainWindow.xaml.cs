688f4f0e038b7fb05c2bf8c9304a8a5f17b4d8cf
francesco-buscicchio <81358077+francesco-buscicchio@users.noreply.github.com>
2023-05-31 18:07:07 +0200

    Management of Exceptions

---
+++ Management of Exceptions/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Management_of_Exceptions
+{
+    /// <summary>
+    /// Logica di interazione per MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            String s = null;
+            //TRY CATCH CLASSICO
+            try
+            {
+                s.Trim();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error in MainWindow component: " + ex.Message);
+            }
+
+            //DispatcherUnhandledException
+            s.Trim();
+        }
+    }
+}
