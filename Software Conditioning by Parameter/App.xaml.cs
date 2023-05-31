7c4ae126d848ee1576d5a0fe3781e92f4950ff23
francesco-buscicchio <81358077+francesco-buscicchio@users.noreply.github.com>
2023-05-31 17:03:11 +0200

    Starting Project

---
+++ Software Conditioning by Parameter/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Software_Conditioning_by_Parameter
+{
+    /// <summary>
+    /// Logica di interazione per App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private void ApplicationStartup(object sender, StartupEventArgs e)
+        {
+            if(e.Args.Length > 0)
+            {
+                MessageBox.Show(e.Args[0]);
+            }
+            else {
+                MainWindow mw = new MainWindow();
+                mw.Show();
+            }
+        }
+    }
+}
